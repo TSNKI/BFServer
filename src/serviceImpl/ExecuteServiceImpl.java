cd01ad6dd82c528d36fa931d3b50e685e8007636
TSNKI <929509707@qq.com>
2016-05-28 16:12:39 +0800

    解释器
    实现了BF的解释，经测试“Hello World!”、加法和乘法均可行。

--- src/serviceImpl/ExecuteServiceImpl.java
+++ src/serviceImpl/ExecuteServiceImpl.java
@@ -2,6 +2,7 @@
 package serviceImpl;
 
 import java.rmi.RemoteException;
+import java.util.ArrayList;
 
 import service.ExecuteService;
 import service.UserService;
@@ -14,7 +15,70 @@ public class ExecuteServiceImpl implements ExecuteService {
 	@Override
 	public String execute(String code, String param) throws RemoteException {
 		// TODO Auto-generated method stub
-		return null;
+
+				// Transfer code into an ArrayList.
+				char[] codes = code.toCharArray();
+
+				// Transfer param into characters.
+				char[] params = param.toCharArray();
+
+				int[] stack = new int[1024];
+				int stackPtr = 0;
+				ArrayList<Integer> loopPtr = new ArrayList<Integer>();
+				int paramPtr = 0;
+				int codePtr = 0;
+				String res = "";
+
+				while (codePtr < codes.length) {
+					switch (codes[codePtr]) {
+					case ',':
+						stack[stackPtr] = params[paramPtr++];
+						codePtr++;
+						break;
+					case '.':
+						res += (char) stack[stackPtr];
+						codePtr++;
+						break;
+					case '+':
+						stack[stackPtr]++;
+						codePtr++;
+						break;
+					case '-':
+						stack[stackPtr]--;
+						codePtr++;
+						break;
+					case '>':
+						stackPtr++;
+						codePtr++;
+						break;
+					case '<':
+						stackPtr--;
+						codePtr++;
+						break;
+					case '[':
+						if (stack[stackPtr] == 0) {
+							codePtr++;
+							while (codes[codePtr] != ']') {
+								codePtr++;
+							}
+							codePtr++;
+						} else {
+							loopPtr.add(codePtr);
+							codePtr++;
+						}
+						break;
+					case ']':
+						if (stack[stackPtr] != 0) {
+							codePtr = loopPtr.get(loopPtr.size() - 1);
+						} else {
+							codePtr++;
+							loopPtr.remove(loopPtr.size() - 1);
+						}
+						break;
+					}
+				}
+
+				return res;
 	}
 
 }
