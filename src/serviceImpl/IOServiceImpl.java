8c04374f7602bafd6a471f5f293dd6354aef16ef
TSNKI <929509707@qq.com>
2016-05-29 15:03:24 +0800

    初步实现IO
    初步实现 读取文件、读取指定用户的文件列表 功能

--- src/serviceImpl/IOServiceImpl.java
+++ src/serviceImpl/IOServiceImpl.java
@@ -1,13 +1,15 @@
 package serviceImpl;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
 import service.IOService;
 
-public class IOServiceImpl implements IOService{
-	
+public class IOServiceImpl implements IOService {
+
 	@Override
 	public boolean writeFile(String file, String userId, String fileName) {
 		File f = new File(userId + "_" + fileName);
@@ -25,14 +27,34 @@ public class IOServiceImpl implements IOService{
 
 	@Override
 	public String readFile(String userId, String fileName) {
-		// TODO Auto-generated method stub
-		return "OK";
+		File f = new File(userId + "_" + fileName);
+		String res = "";
+		String temp = null;
+		try {
+			FileReader fr = new FileReader(f);
+			BufferedReader br = new BufferedReader(fr);
+			while ((temp = br.readLine()) != null) {
+				res += temp + "\n";
+			}
+			br.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.out.println("File does not exist!");
+		}
+		return res;
 	}
 
 	@Override
 	public String readFileList(String userId) {
-		// TODO Auto-generated method stub
-		return "OK";
+		File f = new File("./");
+		File[] files = f.listFiles();
+		String res = "";
+		for (int i = 0; i < files.length; i++) {
+			if (files[i].getName().startsWith(userId)) {
+				res += files[i].getName() + "\n";
+			}
+		}
+		return res;
 	}
-	
+
 }
